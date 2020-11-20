f0c188026c5b8f686b1b142bda952183c3465d4a
Dominik Both <d.both@volzconsulting.de>
2020-11-20 21:30:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// Example Air Quality Sensor Plugin
+
+module.exports = (api) => {
+  api.registerAccessory('SevSensorPlugin', SevSensorAccessory);
+};
+
+class SevSensorAccessory {
+
+  constructor(log, config, api) {
+      this.log = log;
+      this.config = config;
+      this.api = api;
+
+      this.Service = this.api.hap.Service;
+      this.Characteristic = this.api.hap.Characteristic;
+
+      // extract name from config
+      this.name = config.name;
+
+      // create a new Air Quality Sensor service
+      this.service = new this.Service(this.Service.AirQualitySensor);
+
+      // create handlers for required characteristics
+      this.service.getCharacteristic(this.Characteristic.AirQuality)
+        .on('get', this.handleAirQualityGet.bind(this));
+
+  }
+
+  /**
+   * Handle requests to get the current value of the "Air Quality" characteristic
+   */
+  handleAirQualityGet(callback) {
+    this.log.debug('Triggered GET AirQuality');
+
+    // set this to a valid value for AirQuality
+    const currentValue = 1;
+
+    callback(null, currentValue);
+  }
+
+
+}
